7fe820d1cd117756d66c0d114fe5eb16bc652af9
Tina SONG <alan00035@hotmail.com>
2022-03-06 17:39:08 +1100

    init

---
+++ practise/subarray.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class subarray{
+
+    public static void main(String[] args) {
+       int[] arr = {1,-2,4,-5,1};
+       int length = 5;
+      
+       int count =0 ;
+
+       for (int i =0; i < length; i++){
+         int sum =0;
+          for(int k=i; k< length; k++){
+              sum += arr[k];
+            //   System.out.println(sum);
+             if(sum<0)
+                 count++ ;
+
+          }
+         
+          
+       }
+       System.out.println(count);
+    }
+    
+       
+
+
+    }
+
+
+
+
+
+
+
+// https://www.hackerrank.com/challenges/java-negative-subarray/problem?isFullScreen=true
