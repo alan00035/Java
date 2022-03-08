339cab613caffd066195059a817bb3da9187a80e
Tina SONG <alan00035@hotmail.com>
2022-03-08 22:17:49 +1100

    javalist

---
+++ practise/javaList.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class javaList {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int length = input.nextInt();
+        List l = new ArrayList<>();
+        
+        for (int i =0; i<length; i++){    
+            l.add(input.nextInt());
+        }
+        
+        int request = input.nextInt();
+        
+        for(int i =0; i< request; i++){
+            String s = input.next();
+            if(s.equalsIgnoreCase("Insert")){
+                int index = input.nextInt();
+                int obj = input.nextInt();
+                l.add(index,obj);
+                // System.out.println(l);
+            }
+                
+            else 
+                l.remove(input.nextInt());
+                // System.out.println(input.nextInt());
+            
+        }
+        for (Object o: l)
+        System.out.print(o+" ");
+        
+        input.close();
+    }
+    
+}
