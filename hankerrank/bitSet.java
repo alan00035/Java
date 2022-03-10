c7a6dacc63a11851e3f6dd56f48d741279e8bddc
Tina SONG <alan00035@hotmail.com>
2022-03-10 20:58:54 +1100

    bitset

--- hankerrank/bitSet.java
+++ hankerrank/bitSet.java
@@ -1,4 +1,35 @@
+import java.util.BitSet;
+
 public class bitSet {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int length = input.nextInt();
+        int operations = input.nextInt();
+        
+        BitSet set1 = new BitSet(length);
+        BitSet set2 = new BitSet(length);
+        
+        while(operations >0){
+            String op = input.next();
+            int setInt1 = input.nextInt();
+            int setInt2 = input.nextInt();
+            
+            operations--; 
+            switch (op){
+                case "AND": if(setInt1==1) set1.and(set2); else set2.and(set1); break;
+                case "OR": if(setInt1==1) set1.or(set2);else set2.or(set1); break;
+                case "XOR": if(setInt1==1) set1.xor(set2);else set2.xor(set1); break;
+                case "SET": if(setInt1 ==1) set1.set(setInt2, true); else set2.set(setInt2,true);break;
+                case "FLIP": if (setInt1==1) set1.flip(setInt2); else set2.flip(setInt2);break;
+            }
+              
+            System.out.println(set1.cardinality()+ " " +set2.cardinality());
+        }
+        
+    }
+    }
+    
     
 }
 
