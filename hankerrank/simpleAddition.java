b220a5f16c41bcc7d33c1f04f5f366121b664e90
Tina SONG <alan00035@hotmail.com>
2022-03-12 20:56:13 +1100

    varags

---
+++ hankerrank/simpleAddition.java
@@ -0,0 +1,15 @@
+
+public class simpleAddition {
+
+    class Add{
+        void add (int ... args){
+            int sum =0;
+            for(int i:args){
+                sum +=i;
+    
+            }
+            // for(int  i=0; i<args.length; i++ )
+        }
+    }
+    
+}
