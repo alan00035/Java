c4c8a258a334f906cba9e1d3a7e7a20ca5543a03
Tina SONG <alan00035@hotmail.com>
2022-03-22 21:38:05 +1100

    create class

--- priorityQueue.java
+++ priorityQueue.java
@@ -1,3 +1,5 @@
+import java.util.*;
+import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
 
@@ -5,77 +7,84 @@ class Student{
     int id;  String name;  double cgpa;
 
     Student(int id, String name, double cgpa){
+        super();
         this.id =id;
         this.name=name;
         this.cgpa=cgpa;
+
     }
 
-    String getName(){
-        return name;
+    public int getId() {
+        return this.id;
     }
 
-    double getCGPA(){
-        return cgpa;
+    public void setId(int id) {
+        this.id = id;
     }
-    
-    public int compareTo(Student student){
-        if(this.getCGPA() > student.getCGPA()){
-            return 1;
-        }else if(this.getCGPA()< student.getCGPA()){
-            return -1;
-        }else {
-            return 0;
-        }
+
+    public String getName() {
+        return this.name;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getCgpa() {
+        return this.cgpa;
+    }
+
+    public void setCgpa(double cgpa) {
+        this.cgpa = cgpa;
+    }
+
+    @Override
+    public String toString() {
+
+        return super.toString();
+    }
+
 }
 
 
 class Priorities {
     List<Student> getStudents(List<String> events){
-        PriorityQueue queue = new PriorityQueue<>();
+        PriorityQueue<Student> queue = new PriorityQueue<>();
         Student student;
         String[] array = new String[3];
 
            for(String str: events){
                 array = str.split(" ");
                 if(!array[0].equals("SERVED"))
-                    queue.add(new Student(Integer.parseInt(array[3]),
-                                                 array[1],
-                                                 Double.parseDouble(array[2])));
-
-                       
-                   System.out.println(queue.remove());
-    // }
-  
-        
+                    queue.add(new Student(Integer.parseInt(array[3]),array[1],Double.parseDouble(array[2]))); 
     }
      return null;
 }
-
 }
+
 public class priorityQueue {
     // private final static Scanner scan = new Scanner(System.in);
     private final static Priorities priorities = new Priorities();
     
     public static void main(String[] args) {
-        int totalEvents = Integer.parseInt(scan.nextLine());    
-        List<String> events = new ArrayList<>();
+        // int totalEvents = Integer.parseInt(scan.nextLine());    
+        // List<String> events = new ArrayList<>();
         
-        while (totalEvents-- != 0) {
-            String event = scan.nextLine();
-            events.add(event);
-        }
+        // while (totalEvents-- != 0) {
+        //     String event = scan.nextLine();
+        //     events.add(event);
+        // }
         
-        List<Student> students = priorities.getStudents(events);
+        // List<Student> students = priorities.getStudents(events);
         
-        if (students.isEmpty()) {
-            System.out.println("EMPTY");
-        } else {
-            for (Student st: students) {
-                System.out.println(st.getName());
-            }
-        }
-    }
+    //     if (students.isEmpty()) {
+    //         System.out.println("EMPTY");
+    //     } else {
+    //         for (Student st: students) {
+    //             System.out.println(st.getName());
+    //         }
+    //     }
+    // }
 }
 
 
