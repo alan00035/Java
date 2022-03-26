234a6cafab1015cd816f678dda6245c4cd00cf8c
Tina SONG <alan00035@hotmail.com>
2022-03-26 15:15:30 +1100

    update

--- lambda.java
+++ lambda.java
@@ -1,12 +1,19 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.StringTokenizer;
 
-interface functionLambda{
-    boolean isOdd();
-    boolean isPrime();
-    boolean isPalindrome();
+interface checkOdd{
+    boolean isOdd(int i);
+}
+
+interface checkPrime{
+    boolean isPrime(int i);
+}
+
+interface checkPalindrome{
+    boolean isPalindrome(int i);
 }
 
 public class lambda {
@@ -14,16 +21,23 @@ public class lambda {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(bf.readLine());
         
-        while(T-- >0){
+        while(T --> 0){
             String str = bf.readLine();
-            StringTokenizer token = new StringTokenizer(str," ");
-            if(token.nextToken()!=null){
+            StringTokenizer token = new StringTokenizer(str);
                 int check = Integer.parseInt(token.nextToken());
                 int num = Integer.parseInt(token.nextToken());
+            if(check ==1){
+                checkOdd co =(int i) -> { i%2 !=0};
+                System.out.println(co.isOdd( num) ? "ODD" : "EVEN");
+                
+            }else if(check ==2){
+                checkPrime cp = (int i) -> {new BigInteger(String.valueOf(i)).isProbablePrime(1);
+                System.out.println(cp.isPrime( num) ? "PRIME" : "COMPOSITE"); } 
+            }else {
+                
+            }
+            
         }
-        
-    }
-
     
     
 }
