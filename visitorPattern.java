9e52f2b136f867588dfd342be25b9032dae97972
Tina SONG <alan00035@hotmail.com>
2022-03-29 17:49:01 +1100

    tree

--- visitorPattern.java
+++ visitorPattern.java
@@ -10,6 +10,39 @@ abstract class Tree{
     private int value;
     private Color color;
     private int depth;
+
+    Tree(){};
+    Tree(int value, Color color, int depth){
+        super();
+        this.value=value;
+        this.color=color;
+        this.depth=depth;
+    };
+    public int getValue() {
+        return this.value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public Color getColor() {
+        return this.color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public int getDepth() {
+        return this.depth;
+    }
+
+    public void setDepth(int depth) {
+        this.depth = depth;
+    }
+
+  
 }
 
 enum Color{
