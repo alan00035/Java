cf54c83192622164431b4613a4ebb66695695595
Tina SONG <alan00035@hotmail.com>
2022-03-30 17:43:26 +1100

    add tree node & leaf class

--- visitorPattern.java
+++ visitorPattern.java
@@ -15,4 +15,45 @@ class Tree{
     int value;
     Color color;
     int depth;
+
+    Tree(int value, Color c, int depth){
+        this.value=value;
+        this.color=c;
+        this.depth=depth;
+    }
+
+    int getValue(){
+        return value;
+    }
+
+    Color getColor(){
+        return color;
+    }
+
+    int getDepth(){
+        return depth;
+    }
+}
+
+class TreeNode extends Tree{
+
+    TreeNode(int value, Color c, int depth) {
+        super(value, c, depth);
+        //TODO Auto-generated constructor stub
+    }
+
+}
+
+class TreeLeaf extends Tree{
+
+    TreeLeaf(int value, Color c, int depth) {
+        super(value, c, depth);
+        //TODO Auto-generated constructor stub
+    }
+
+}
+interface action {
+    int getResult();
+    void visitNode(TreeNode node);
+    void visitLeaf(TreeLeaf leaf);
 }
