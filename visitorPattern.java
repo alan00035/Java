553d7a86795599d5f80c23f67c5d9428f5dc8115
Tina SONG <alan00035@hotmail.com>
2022-03-30 17:17:06 +1100

    update

--- visitorPattern.java
+++ visitorPattern.java
@@ -1,51 +1,18 @@
-public class visitorPattern {
+import org.w3c.dom.Node;
+
+public class visitorPattern{
     public static void main(String[] args) {
-        
+
     }
-    
-}
-
-
-abstract class Tree{
-    private int value;
-    private Color color;
-    private int depth;
-
-    Tree(){};
-    Tree(int value, Color color, int depth){
-        super();
-        this.value=value;
-        this.color=color;
-        this.depth=depth;
-    };
-    public int getValue() {
-        return this.value;
-    }
-
-    public void setValue(int value) {
-        this.value = value;
-    }
-
-    public Color getColor() {
-        return this.color;
-    }
-
-    public void setColor(Color color) {
-        this.color = color;
-    }
-
-    public int getDepth() {
-        return this.depth;
-    }
-
-    public void setDepth(int depth) {
-        this.depth = depth;
-    }
-
-  
 }
 
 enum Color{
-    RED, GREEN
+    RED, GREEN;
 }
-// https://www.hackerrank.com/challenges/java-vistor-pattern/problem?isFullScreen=true
+
+class Tree{
+
+    int value;
+    Color color;
+    int depth;
+}
