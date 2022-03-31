4fa5c9b334d87647a049f2e15b64ff6dab688670
Tina SONG <alan00035@hotmail.com>
2022-03-31 14:48:39 +1100

    update

--- node/Node.java
+++ node/Node.java
@@ -1,29 +1,44 @@
 package node;
 
+import java.util.ArrayList;
+
 public class Node<T>{
 
     private T data;
-    private Node next;
+    // private Node next;
+    private ArrayList<Node> neigbors;
 
-    public T getData() {
-        return this.data;
-    }
-
-    public void setData(T data) {
+    Node(T date){
         this.data = data;
+        neigbors=new ArrayList<Node>();
     }
 
-    public Node getNext() {
-        return this.next;
-    }
 
-    public void setNext(Node next) {
-        this.next = next;
-    }
+    public T getData() {return this.data;}
 
-    public Node(T data, Node nx){
-        this.data = data;
-        this.next = nx;
-    }
+    public void setData(T data) {this.data = data;}
+
+
+    //  use arraylist to get neghbours node
+
+    
+
+    // public Node getNext() {
+    //     return this.next;
+    // }
+
+    // public void setNext(Node next) {
+    //     this.next = next;
+    // }
+
+    // public Node(T data, Node nx){
+    //     this.data = data;
+    //     this.next = nx;
+    // }
+
+
+    
+
+
 
 }
