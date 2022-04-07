eaa962fa04cbe0ecebfe906795b3b6e6fa01194b
Tina SONG <alan00035@hotmail.com>
2022-04-07 19:39:36 +1000

    twosets

---
+++ betweenTwoSets.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class betweenTwoSets {
+    public static void main(String[] args) {
+        int[] a ={2,6};
+        int[] b ={24,36};
+
+        List<Integer> between = new ArrayList<>();
+
+        int x = a[a.length-1];
+        int y = b[b.length-1];
+
+        int count =0;
+        
+        while()
+        int add = x*2;
+        int new = add*2;
+
+        
+
+    }
+    
+}
