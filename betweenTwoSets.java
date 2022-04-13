e640641664e5a5d53c297c6416b80c36d7d27e99
Tina SONG <alan00035@hotmail.com>
2022-04-13 17:24:16 +1000

    twoset

--- betweenTwoSets.java
+++ betweenTwoSets.java
@@ -6,18 +6,20 @@ public class betweenTwoSets {
         int[] a ={2,6};
         int[] b ={24,36};
 
-        List<Integer> between = new ArrayList<>();
-
-        int x = a[a.length-1];
-        int y = b[b.length-1];
-
-        int count =0;
-        
-        while()
-        int add = x*2;
-        int new = add*2;
- 
-        
+        int cnt =0;
+        for(int i=1; i<=100;i++){
+            boolean ok =true;
+            for(int j=0; j<a.length && ok ; j++){
+                if(i%a[j] !=0)
+                    ok =false;
+            }
+            for(int j=0; j<b.length &&ok; j++){
+                if(b[j]%i !=0) 
+                    ok =false;
+            }
+            if(ok) cnt ++;
+        }
+            System.out.println(cnt);
 
     }
     
