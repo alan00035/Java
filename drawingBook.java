366b7217e0589dfc383472b795f34582eb9473bb
Tina SONG <alan00035@hotmail.com>
2022-04-15 08:53:31 +1000

    update

--- drawingBook.java
+++ drawingBook.java
@@ -2,12 +2,19 @@ public class drawingBook {
     public static void main(String[] args) {
         //page 1 is on the right, last page is on the left;
 
-        int n = 5;
-        int p = 3;
+        int n = 6; //book total pages
+        int p = 2; // page position
+
 
         int back =0;
-        int front =0;
-
+        int front =Math.abs((p-n+1)/2);
+        if(n%2 ==0){
+            back = Math.abs((n-1-p)/2);
+            System.out.println(front>back ? back:front); 
+        }
+        else
+            back = Math.abs(n/2); 
+            System.out.println(front>back ? back:front);
 
 
     }   
