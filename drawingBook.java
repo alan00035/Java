1deaee19cfd52b3fbfdd658443f76cec6282748b
Tina SONG <alan00035@hotmail.com>
2022-04-14 19:36:52 +1000

    book

--- drawingBook.java
+++ drawingBook.java
@@ -1,6 +1,15 @@
 public class drawingBook {
     public static void main(String[] args) {
-        
+        //page 1 is on the right, last page is on the left;
+
+        int n = 5;
+        int p = 3;
+
+        int back =0;
+        int front =0;
+
+
+
     }   
 }
 
