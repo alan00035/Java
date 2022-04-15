7cdd1f1ffb1f634037e439f0a40c0dd9b2ed333d
Tina SONG <alan00035@hotmail.com>
2022-04-15 13:56:38 +1000

    udpate

--- drawingBook.java
+++ drawingBook.java
@@ -2,24 +2,25 @@ public class drawingBook {
     public static void main(String[] args) {
         //page 1 is on the right, last page is on the left;
 
-        int n = 6; //book total pages
-        int p = 2; // page position
+        int n = 37455; //book total pages
+        int p = 29835; // page position
 
 
-        int back =0;
-        int front =Math.abs((n-1-p)/2);
+        
+        int front =p/2;
+        int back = n/2-p/2;
+        System.out.println(front > back ? back: front);
 
-        if (p ==1 || p ==n )
-            System.out.println(0);
-        else
-            if(n%2 ==0){
-                
-                back = Math.abs((n-1-p)/2);
-                System.out.println(front>back ? back:front);
-            }
-            else
-                back = Math.abs((n-p)/2); 
-                System.out.println(front>back ? back:front);
+        // if (p ==1 || p ==n )
+        //     System.out.println(0);
+        // else
+        //     if(n%2 ==0){
+        //         back = Math.abs((n-1-p)/2);
+        //         System.out.println(front>back ? back:front);
+        //     }
+        //     else
+        //         back = Math.abs((n-p)/2); 
+        //         System.out.println(front>back ? back:front);
 
     }   
 }
