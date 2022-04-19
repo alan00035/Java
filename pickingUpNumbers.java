5d257503fdc8eeff243a89140cdc37ab10005ee2
Tina SONG <alan00035@hotmail.com>
2022-04-19 19:54:08 +1000

    update

--- pickingUpNumbers.java
+++ pickingUpNumbers.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map.Entry;
 
 import hankerrank.easy.list;
 
@@ -8,9 +10,23 @@ public class pickingUpNumbers {
     public static void main(String[] args) {
         int [] arr = {1 ,3 ,3 ,4 ,5 ,6};
         
-        List<Integer> sublist = new ArrayList<>();
+        // List<Integer> sublist = new ArrayList<>();
 
-        Arrays.sort(arr);
+        // Arrays.sort(arr);
+        HashMap<Integer,Integer> hm = new HashMap<>();
+
+        for(int i :arr){
+            if(hm.containsKey(i))
+                hm.put(i, hm.get(i)+1);
+            else
+                hm.put(i, 1);
+        }
+        
+        for(Entry<Integer,Integer> entry : hm.entrySet() ){
+            if(entry.getKey()==4)
+                System.out.println(entry.getValue());
+        }
+        
 
         List<Integer> sum = new ArrayList<>();
 
