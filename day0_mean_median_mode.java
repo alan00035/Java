6ee28c6680f49bfe7d3d6f4a8235deb091d02362
Tina <tina.song@anz.com>
2022-04-20 09:34:30 +1000

    add day0

---
+++ day0_mean_median_mode.java
@@ -0,0 +1,7 @@
+public class day0_mean_median_mode{
+    public static void main(String[] args) {
+        
+    }
+}
+
+// https://www.hackerrank.com/challenges/s10-basic-statistics/problem?isFullScreen=true
