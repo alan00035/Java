a594db3cc704179eafb336f0d8bd286d85f6a6d5
Tina <tina.song@anz.com>
2022-04-20 09:33:47 +1000

    clean up

--- hankerrank/medium/oneDArray.java
+++ hankerrank/medium/oneDArray.java
@@ -2,7 +2,7 @@ package hankerrank.medium;
 import java.io.*;
 import java.util.*;
 
-public class Solution {
+public class oneDArray {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */ 
