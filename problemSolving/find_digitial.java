6cd2ebd7fd56a130571b350772a9171380c2d00c
Tina SONG <alan00035@hotmail.com>
2022-04-25 20:14:16 +1000

    update

---
+++ problemSolving/find_digitial.java
@@ -0,0 +1,29 @@
+import java.io.Console;
+
+public class find_digitial {
+    public static void main(String[] args) {
+        int n =1012;
+        char[] ch = String.valueOf(n).toCharArray();
+
+
+        int count=0;
+        
+
+        for(Character c: ch){
+            int a = Integer.parseInt(String.valueOf(c));
+            
+        try {
+            if((n%a)==0) 
+                count++;
+            
+        } catch (Exception e) {
+            //TODO: handle exception
+        }    
+            
+    }
+       
+        System.out.println(count);        
+    }           
+}
+
+// https://www.hackerrank.com/challenges/find-digits/problem?isFullScreen=true
