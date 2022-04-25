6d4812ddd2a80ec6abbaed0d4866e5b5e908374f
Tina SONG <alan00035@hotmail.com>
2022-04-25 12:19:40 +1000

    update

--- circular_array_rotation.java
+++ circular_array_rotation.java
@@ -1,10 +1,39 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class circular_array_rotation {
     public static void main(String[] args) {
-        int[] a={3,4,5};
-        int k=2;
+        int k=1;  //rotation numbers
         int[] queries={1,2};
 
-        int length = a.length;
+
+        List<Integer> source = Arrays.asList(1,2,3);
+        int length = source.size();
+
+        List<Integer> list1 = source.subList(length-k, length);
+        List<Integer> list2 = source.subList(0, k-1);
+        List<Integer> rotaList = new ArrayList<>(list1);
+        rotaList.addAll(list2);    
+
+
+
+        if(k==1){
+            for(int i : queries)
+                System.out.println(source.get(i));
+        }
+        else{
+            for(int i: rotaList)
+                System.out.println(i);
+            
+        }
+        
+
+
+        
+       
  
        
 
---
+++ find_digitial.java
@@ -0,0 +1,7 @@
+public class find_digitial {
+    public static void main(String[] args) {
+        
+    }
+}
+
+// https://www.hackerrank.com/challenges/find-digits/problem?isFullScreen=true
