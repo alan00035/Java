fd0714258080c3b735c29f28fe5d5abd4054a5d5
Tina SONG <alan00035@hotmail.com>
2022-04-28 22:11:23 +1000

    bigInteger

---
+++ problemSolving/extra_long_factorials.java
@@ -0,0 +1,19 @@
+package problemSolving;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public class extra_long_factorials {
+    public static void main(String[] args) {
+        int i=25;
+        BigInteger val = new BigInteger(String.valueOf(1)) ;
+        for(; i>0 ; i--){
+            BigInteger a = new BigInteger(String.valueOf(i));
+            //  val *= i; 
+            val= val.multiply(a);
+        }
+      
+        System.out.println(val);
+    }
+}
+
+// https://www.hackerrank.com/challenges/extra-long-factorials/problem?isFullScreen=true
