bb88156a2fde6979a0c9b93d52c50e350d2bb205
Tina SONG <alan00035@hotmail.com>
2022-05-12 09:22:56 +1000

    update

--- encryption.java
+++ encryption.java
@@ -7,36 +7,36 @@ public class encryption {
     public static void main(String[] args) {
         String s ="if man was meant to stay on the ground god would have given us roots ";
         // String s="haveaniceday";
+        // String s ="clu hlt io";s
         String regex ="\\s+";
 
         String str = s.replaceAll(regex, "");
 
         double root = Math.sqrt(str.length());
 
-        int down = (int)Math.floor(root); //7
-        int up = (int)Math.ceil(root);    //8
+        int up =(int)Math.ceil(root);
+        int down =  (int)Math.floor(root)*up <str.length() ? up: (int)Math.floor(root);
+
+        // System.out.println(down+"  "+up);
 
         List<String> stringList = new ArrayList<>();
         List<String> newList = new ArrayList<>();
     
+        StringBuilder sb = new StringBuilder();
 
-        // for(int i =0; i<str.length()-up; i+=up){
-        //     stringList.add(str.substring(i, i+up));
-            
-        // }
+        int gap = Math.abs(up*down-str.length());
 
-        //     stringList.add(str.substring((down-1)*up, str.length()));
-           
-        // System.out.println(stringList);
-            StringBuilder sb = new StringBuilder();
-
+        for(int n =0; n< up || n<gap; n++){
             for(int i =0; i<str.length(); i+=up){
-           
-                    sb.append(str.charAt(i)).toString();
-            }
+                    sb.append(str.charAt(i+n)).toString();
                     System.out.println(sb);
-    
-            // System.out.println(sb+" ");
+            }
+
+           
+                   
+        }
+
+            // System.out.println(sb);
        
         }
      
