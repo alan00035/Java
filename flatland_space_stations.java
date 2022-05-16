023cd7621bf17a73813931c2ff11f33e4a2c2c03
Tina SONG <alan00035@hotmail.com>
2022-05-17 09:02:23 +1000

    update:

---
+++ flatland_space_stations.java
@@ -0,0 +1,9 @@
+public class flatland_space_stations {
+    public static void main(String[] args) {
+        int n =5; //cites
+        int[] c= {0,4};
+        
+    }
+}
+
+// https://www.hackerrank.com/challenges/flatland-space-stations/problem?isFullScreen=true
