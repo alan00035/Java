15f35ccbbf49cb45ce6686e763cfc0f6b2c8408e
Tina SONG <alan00035@hotmail.com>
2022-05-18 09:01:27 +1000

    update:

--- flatland_space_stations.java
+++ flatland_space_stations.java
@@ -1,8 +1,18 @@
 public class flatland_space_stations {
     public static void main(String[] args) {
-        int n =5; //cites
-        int[] c= {0,4};
-        
+        int n =3; //cites
+        int[] c= {1}; //cites has space station
+
+        int max = 0;
+        for(int j=1; j<c.length; j++){
+            for(int i =0; i<n; i++){
+                int distance = Math.abs(Math.max(c[j-1]-i, c[j]-i)) ;
+                if(distance>max)
+                    max = distance;
+            }
+
+        }
+        System.out.println(max);
     }
 }
 
