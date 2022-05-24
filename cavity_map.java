184537aea0ed1cddc608fe91bcf26fad691a3674
Tina SONG <alan00035@hotmail.com>
2022-05-24 20:49:03 +1000

    update:

--- cavity_map.java
+++ cavity_map.java
@@ -4,25 +4,28 @@ import java.util.List;
 
 public class cavity_map {
     public static void main(String[] args) {
-        String[] g ={"988","191","111"};
+        String[] g ={"1112","1912","1892","1234"};
         List<String> grid = Arrays.asList(g);
-        int n = 3;
+        int n = 4;
         String new_str= "";
         List<String> new_grid = new ArrayList<>();
             
-        for(int i =0; i<n; i++){
+        new_grid.add(grid.get(0));
+        for(int i =1; i<n-1; i++){
             char[] ch = grid.get(i).toCharArray();
-                if(ch[i] == '9' && i!=n-1 && i!=0){
-                    new_str= grid.get(i).replace('9', 'X');       
+                if(ch[i] > ch[i-1] && ch[i] > ch[i+1] && i!=n-1 && i!=0){
+                    new_str= grid.get(i).replace(ch[i], 'X');       
                         new_grid.add(new_str);}
                 else{
                     new_grid.add(grid.get(i));
                 }        
         }
+
+        new_grid.add(grid.get(n-1));
             
-        // for(String s :grid){
-        //     System.out.print(s+" ");
-        // }
+        for(String s :new_grid){
+            System.out.print(s+" ");
+        }
     }
 }
 
