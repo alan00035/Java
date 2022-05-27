f82d68ef996cf028648bf230521776fd23d18107
Tina SONG <alan00035@hotmail.com>
2022-05-27 20:21:32 +1000

    update

---
+++ manasa_and_stones.java
@@ -0,0 +1,7 @@
+public class manasa_and_stones {
+    public static void main(String[] args) {
+        
+    }
+}
+
+// https://www.hackerrank.com/challenges/manasa-and-stones/problem?isFullScreen=true
