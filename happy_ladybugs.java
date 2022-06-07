f12f4d9746226d3f1db6fd7812ff332c404dbf67
Tina SONG <alan00035@hotmail.com>
2022-06-07 17:12:16 +1000

    add

---
+++ happy_ladybugs.java
@@ -0,0 +1,8 @@
+public class happy_ladybugs {
+    public static void main(String[] args) {
+        
+    }
+}
+
+
+// https://www.hackerrank.com/challenges/happy-ladybugs/problem?isFullScreen=true
