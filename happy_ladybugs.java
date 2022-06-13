dae3bbb3187a753abdd85a07df9a5048baaa1df5
Tina SONG <alan00035@hotmail.com>
2022-06-13 16:59:49 +1000

    update

--- happy_ladybugs.java
+++ happy_ladybugs.java
@@ -1,22 +1,31 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map.Entry;
+
+import hankerrank.easy.list;
 
 public class happy_ladybugs {
     public static void main(String[] args) {
         String b = "RBY_YBR";
         int length = b.length();
 
+        ArrayList<Character> list = new ArrayList<>();
+
         HashMap<Character, Integer> hm = new HashMap<>();
         for(char ch: b.toCharArray()){
             if(hm.containsKey(ch))
-                 hm.put(ch, hm.get(ch)+1 );
+                hm.put(ch, hm.get(ch)+1 );
+            else    
+                hm.put(ch, 1) ;  
+        }
 
-        else    
-            hm.put(ch, 1) ;  
-        }
+        for(Entry<Character, Integer> entry : hm.entrySet())
+              if(entry.getValue().equals(1)){
+                 ;
+              }
         
-        if(hm.get('_') != null){
-            
-        }
+        
             
             
 
