51005771c695cac382ae91ea9b85add782709631
Tina SONG <alan00035@hotmail.com>
2022-06-15 11:55:20 +1000

    update

--- happy_ladybugs.java
+++ happy_ladybugs.java
@@ -3,51 +3,62 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map.Entry;
 
-import hankerrank.easy.list;
 
-public class happy_ladybugs {
+public class happy_ladybugs{
     public static void main(String[] args) {
-        String b = "AABCBC";
-        String result = null;
+        String b = "RBY_YBR";
+        int count =0;
 
-        ArrayList<Character> list = new ArrayList<>();
 
-        HashMap<Character, Integer> hm = new HashMap<>();
-        for(char ch: b.toCharArray()){
-            if(hm.containsKey(ch))
-                hm.put(ch, hm.get(ch)+1 );
-            else    
-                hm.put(ch, 1) ;  
         }
+    
+    }
 
-        if(!hm.containsKey('_')){
-            int i ;
-            for(i =1; i<b.length(); i++){
-                if(b.charAt(i) != b.charAt(i-1)){
-                    result = "NO";
-                }
-                else{
-                    result ="YES";
-                }
-            }
-        }
-        else{
-            for(Entry<Character, Integer> entry : hm.entrySet()){
-                if(entry.getValue().equals(1) && !entry.getKey().equals('_')){
-                    result = "NO";
-                    break;
-                }
-                else{
-                    result = "YES";
-                }
-                }
-        } 
-        System.out.println(result);
+
+
+// public class happy_ladybugs {
+//     public static void main(String[] args) {
+//         String b = "AABCBC";
+//         String result = null;
+
+//         ArrayList<Character> list = new ArrayList<>();
+
+//         HashMap<Character, Integer> hm = new HashMap<>();
+//         for(char ch: b.toCharArray()){
+//             if(hm.containsKey(ch))
+//                 hm.put(ch, hm.get(ch)+1 );
+//             else    
+//                 hm.put(ch, 1) ;  
+//         }
+
+//         if(!hm.containsKey('_')){
+//             int i ;
+//             for(i =1; i<b.length(); i++){
+//                 if(b.charAt(i) != b.charAt(i-1)){
+//                     result = "NO";
+//                 }
+//                 else{
+//                     result ="YES";
+//                 }
+//             }
+//         }
+//         else{
+//             for(Entry<Character, Integer> entry : hm.entrySet()){
+//                 if(entry.getValue().equals(1) && !entry.getKey().equals('_')){
+//                     result = "NO";
+//                     break;
+//                 }
+//                 else{
+//                     result = "YES";
+//                 }
+//                 }
+//         } 
+//         System.out.println(result);
             
 
         
-}
-}
+// }
+// }
 
 
 // https://www.hackerrank.com/challenges/happy-ladybugs/problem?isFullScreen=true
