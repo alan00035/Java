10d1c8160a74de2662e55697a9f18a4fd72426e5
Tina SONG <alan00035@hotmail.com>
2022-06-17 08:59:05 +1000

    update:

--- bigger_is_greater.java
+++ bigger_is_greater.java
@@ -1,47 +1,10 @@
-import javax.lang.model.util.ElementScanner6;
 
 public class bigger_is_greater{
     public static void main(String[] args) {
         String w = "dcba"; //zzzayybbaa
         char[] ch = w.toCharArray();
-        Character temp =null;    
-        String ans = null;   
-
-        int n = w.length()-1;
-
-        while(n>=1 && ch[n-1] >=ch[n])
-            n--;
-        if(n <=0)
-            ans="no answer";
-            
-        int i = w.length()-1;    
-
-        while(ch[i] <=ch[n-1]) // index -1 out of bounds for length 1
-            i--;
         
-            temp =ch[n-1];
-            ch[n-1] = ch[i];
-            ch[i] = temp;
 
-
-            
-        i= w.length()-1; 
-        while(n<i){
-            {
-                temp = ch[n];
-                ch[n] = ch[i];
-                ch[i] = temp;
-                n++;
-                i--;
-            }
-        }
-
-         ans = new String(ch);
-         
-
-       
-        System.out.println(ans);
-        // System.out.println("b".compareTo("d"));
 ;
     }
 
@@ -51,3 +14,5 @@ public class bigger_is_greater{
 // https://www.hackerrank.com/challenges/bigger-is-greater/problem?isFullScreen=true
 
 // explanation of lexicographical algorithm :  https://www.nayuki.io/page/next-lexicographical-permutation-algorithm
+
+// https://programs.programmingoneonone.com/2021/03/hackerrank-bigger-is-greater-solution.html
