9c58eb07d37ef5011ad616f77bfe8ae6bc7f78ca
Tina SONG <alan00035@hotmail.com>
2022-06-19 20:47:06 +1000

    update

--- bigger_is_greater.java
+++ bigger_is_greater.java
@@ -16,3 +16,7 @@ public class bigger_is_greater{
 // explanation of lexicographical algorithm :  https://www.nayuki.io/page/next-lexicographical-permutation-algorithm
 
 // https://programs.programmingoneonone.com/2021/03/hackerrank-bigger-is-greater-solution.html
+
+// plan to arrive after 8th Oct 2022
+
+// its A family visit, possible to come to visit once or more than one time . However, nothing has final decide yet and it will depends on the current circumstance. 
