8e0858fe7ade1ef669e60503b353fba7f9477050
Tina SONG <alan00035@hotmail.com>
2022-06-21 11:47:11 +1000

    update: git push

--- bigger_is_greater.java
+++ bigger_is_greater.java
@@ -3,8 +3,7 @@ public class bigger_is_greater{
     public static void main(String[] args) {
         String w = "dcba"; //zzzayybbaa
         char[] ch = w.toCharArray();
-        
-
+    
 ;
     }
 
@@ -16,4 +15,3 @@ public class bigger_is_greater{
 // explanation of lexicographical algorithm :  https://www.nayuki.io/page/next-lexicographical-permutation-algorithm
 
 // https://programs.programmingoneonone.com/2021/03/hackerrank-bigger-is-greater-solution.html
-
