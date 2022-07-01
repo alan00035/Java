44d27cdf6c0f7b94599e38a16c9693f9e6bec0a7
Tina SONG <alan00035@hotmail.com>
2022-07-01 19:51:43 +1000

    update

---
+++ the_grid_search.java
@@ -0,0 +1,7 @@
+public class the_grid_search {
+    public static void main(String[] args) {
+        
+    }
+}
+
+// https://www.hackerrank.com/challenges/the-grid-search/problem?isFullScreen=true
