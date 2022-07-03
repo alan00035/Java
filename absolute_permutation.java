77d5b8e7ed68a41c89de7a12d59254ba50533b39
Tina SONG <alan00035@hotmail.com>
2022-07-03 20:51:24 +1000

    update

--- absolute_permutation.java
+++ absolute_permutation.java
@@ -1,5 +1,8 @@
 public class absolute_permutation {
     public static void main(String[] args) {
-        
+        int n =4, k =2;
+
     }
 }
+
+// https://www.hackerrank.com/challenges/absolute-permutation/problem?isFullScreen=true
