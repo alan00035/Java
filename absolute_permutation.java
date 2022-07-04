6f81c692b043efb9188bd1fa7d52549cf60fd8b5
Tina SONG <alan00035@hotmail.com>
2022-07-04 19:33:07 +1000

    array

--- absolute_permutation.java
+++ absolute_permutation.java
@@ -2,6 +2,21 @@ public class absolute_permutation {
     public static void main(String[] args) {
         int n =4, k =2;
 
+        int[] a = new int[n+1];
+
+        int i ;
+        for(i=1; i<n+1; i++){
+            if(Math.abs(a[i]-i) ==k){
+                a[i] =k+i;
+            }
+        }
+
+        for(int j:a){
+            System.out.print(j+" ");
+        }
+        
+
+       
     }
 }
 
