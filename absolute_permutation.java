7ef172f0a98408aad8721f48e3238f4b50f0493b
Tina SONG <alan00035@hotmail.com>
2022-07-07 19:35:24 +1000

    add

--- absolute_permutation.java
+++ absolute_permutation.java
@@ -1,18 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class absolute_permutation {
     public static void main(String[] args) {
-        int n =4, k =2;
+        int n =10, k =9;
 
+        
         int[] a = new int[n];
 
+
+        List<Integer> list = new ArrayList<>();
+
         if(k>0 && n%(2*k) !=0){
-            a=null;
+            list.add(-1);
+
         }
 
         for(int i =1; i<=n; i++){
             a[i-1] = i+k;
-            // if(k>0 && i%k ==1){
-            //     k *=-1;
-            // }
+            if(k>0 && i%k ==0){
+                k *=-1;
+            }
         }
 
         for(int m : a){
