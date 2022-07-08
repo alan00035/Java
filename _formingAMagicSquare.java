20b6625bc0ca6545753bfaacc7b4fc0eef39f9a9
Tina SONG <alan00035@hotmail.com>
2022-07-08 17:05:46 +1000

    clean up

--- _formingAMagicSquare.java
+++ _formingAMagicSquare.java
@@ -1,10 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class _formingAMagicSquare {
    public static void main(String[] args) {
