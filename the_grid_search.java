177563d942f47541031ec7573d41ec0c41090119
Tina SONG <alan00035@hotmail.com>
2022-07-12 15:17:42 +1000

    array search

--- the_grid_search.java
+++ the_grid_search.java
@@ -1,5 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class the_grid_search {
     public static void main(String[] args) {
+        String a = "1234567890";
+        String b ="0987654321";
+        String c ="1111111111";
+        String d = "1111111111";
+        String e ="2222222222";
+        List<String> list = new ArrayList<>();
+        list.add(a); list.add(b);list.add(c);list.add(d);list.add(e);
         
     }
 }
