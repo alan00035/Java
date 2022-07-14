c7a3ac441c293a647003b6fc9b57b23a15df0d4d
Tina SONG <alan00035@hotmail.com>
2022-07-15 09:16:56 +1000

    loop

--- the_bomberman_game.java
+++ the_bomberman_game.java
@@ -5,13 +5,24 @@ public class the_bomberman_game {
     public static void main(String[] args) {
         int n =3; //seconds tp simiulate
         List<String> grid = new ArrayList<>();
-        String a1=".......";String a2 = "...O...";String a3 ="....O.."; String a4 ="......."; String a5 ="OO.....";String a6="OO.....";
+        String a1=".......";String a2 = "...0...";String a3 ="....0.."; String a4 ="......."; String a5 ="00.....";String a6="00.....";
 
-        grid.add(a1);grid.add(a2);grid.add(a3);grid.add(a4);grid.add(a5);grid.add(a6)
+        grid.add(a1);grid.add(a2);grid.add(a3);grid.add(a4);grid.add(a5);grid.add(a6);
 
         //  1)gird list is the initial stage with bomb 2)  sec one - do nothing
         //  3) sec two - plants bombs in all cells; 4) sec three - initial bomb explored
         //  then repeat 3 & 4
+
+        int x,y;
+        for(x=0; x< grid.size(); x++){
+          for(y=0; y<grid.get(0).length(); y++){
+            if(grid.get(x).charAt(y) == '0'){
+                System.out.println(y);
+            }
+                
+          }
+            
+        }
     ;
 
     }
