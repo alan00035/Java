914284649e1a1da082b4bd43d7567b2786e77aa9
Tina SONG <alan00035@hotmail.com>
2022-08-03 16:12:50 +1000

    string

--- super_reduced_string.java
+++ super_reduced_string.java
@@ -2,39 +2,52 @@ import java.util.HashMap;
 import java.util.Map.Entry;
 
 public class super_reduced_string {
-    public static void main(String[] args) {
-        String str = "aaabccddd";
+        public static void main(String[] args) {
+            String s = "zztqooauhujtmxnsbzpykwlvpfyqijvdhuhiroodmuxiobyvwwxupqwydkpeebxmfvxhgicuzdealkgxlfmjiucasokrdznmtlwh";
 
-        HashMap<Character, Integer> hm = new HashMap<>();
+            int count =0;
+            for(char ch : s.toCharArray()){
+                
+            }
 
-        for(int i =0; i<str.length(); i++){
-            Character ch = str.charAt(i);
-            if(hm.containsKey(ch)){
-               hm.put(ch, hm.get(ch)+1);
-            }
-            else{
-                hm.put(ch, 1);
-            }
-        }
-        
-        StringBuilder sb = new StringBuilder();
-
-        for(Entry<Character,Integer> entry : hm.entrySet() ){
-            if(entry.getValue()%2 ==1){
-                sb.append(entry.getKey());
-            }
         }
 
-        String ans;
-        if(sb.length() ==0)
-            ans = "Empty String";
-        
-        else
-            ans = sb.toString();
-        
-
-            System.out.println(ans);
-        }
 }
+
+
+    // public static void main(String[] args) {
+    //     String str = "zztqooauhujtmxnsbzpykwlvpfyqijvdhuhiroodmuxiobyvwwxupqwydkpeebxmfvxhgicuzdealkgxlfmjiucasokrdznmtlwh";
+
+    //     HashMap<Character, Integer> hm = new HashMap<>();
+
+    //     for(int i =0; i<str.length(); i++){
+    //         Character ch = str.charAt(i);
+    //         if(hm.containsKey(ch)){
+    //            hm.put(ch, hm.get(ch)+1);
+    //         }
+    //         else{
+    //             hm.put(ch, 1);
+    //         }
+    //     }
+        
+    //     StringBuilder sb = new StringBuilder();
+
+    //     for(Entry<Character,Integer> entry : hm.entrySet() ){
+    //         if(entry.getValue()%2 ==1){
+    //             sb.append(entry.getKey());
+    //         }
+    //     }
+
+    //     String ans;
+    //     if(sb.length() ==0)
+    //         ans = "Empty String";
+        
+    //     else
+    //         ans = sb.toString();
+        
+
+    //         System.out.println(ans);
+    //     }
+
 
 // https://www.hackerrank.com/challenges/reduced-string/problem?isFullScreen=true
