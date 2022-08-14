501c8dcec1257a332f6600c83876dc0d7329e07e
Tina SONG <alan00035@hotmail.com>
2022-08-14 21:26:21 +1000

    statistics

---
+++ day_0_mean_median_and_mode.java
@@ -0,0 +1,13 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class day_0_mean_median_and_mode {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in);
+        int total = Integer.parseInt(br.readLine().trim());
+
+    }
+}
+
+// https://www.hackerrank.com/challenges/s10-basic-statistics/problem?isFullScreen=true
