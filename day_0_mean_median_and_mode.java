c2ac9c89c516bd92afccc82992525951c9560aff
Tina SONG <alan00035@hotmail.com>
2022-08-18 21:53:44 +1000

    mode

--- day_0_mean_median_and_mode.java
+++ day_0_mean_median_and_mode.java
@@ -14,7 +14,7 @@ public class day_0_mean_median_and_mode {
         int maxValue=0;
         int total = 10;
         // int[] arr = new int[total];
-       int[] arr={64630, 11735, 14216, 99233, 14470,4978,4978,4978, 73429, 38120, 51135, 67060};
+       int[] arr={64630, 11735, 14216, 99233, 14470,4978,73429, 38120, 51135, 67060};
 
        int mean = Arrays.stream(arr).sum()/total;
        int median = (arr[total/2] + arr[(total/2)+1])/2;
@@ -32,10 +32,14 @@ public class day_0_mean_median_and_mode {
        for(Entry<Integer, Integer> entry: hm.entrySet()){
          if(entry.getValue() >max)
             max = entry.getValue();
-            
+          if(entry.getValue()==max && max !=1)
+            maxValue = entry.getKey();
+          else if(max ==1)
+            Arrays.sort(arr);
+            maxValue = arr[0];
        }
        
-       System.out.println(maxValue);
+       System.out.println(max + " "+maxValue);
        
 
       
