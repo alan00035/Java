707d5d2fd512e7c97a3ef51d201d6b68721b6b61
Tina SONG <alan00035@hotmail.com>
2022-08-20 19:49:59 +1000

    add scanner

--- day0_weighted_mean.java
+++
@@ -1,8 +0,0 @@
-public class day0_weighted_mean {
-    public static void main(String[] args) {
-        
-    }
-}
-
-
-// https://www.hackerrank.com/challenges/s10-weighted-mean/problem?isFullScreen=true
