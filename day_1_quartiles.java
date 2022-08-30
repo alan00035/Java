9b5877a1cd2756787a4ec3da968e87678235bc89
Tina SONG <alan00035@hotmail.com>
2022-08-30 21:27:27 +1000

    quartiles

--- day_1_quartiles.java
+++ day_1_quartiles.java
@@ -6,22 +6,31 @@ import java.util.List;
 public class day_1_quartiles {
     public static void main(String[] args) {
         Integer[] arr1 ={5,7,1,3};
-
+        
         List<Integer> arr = Arrays.asList(arr1);
         List<Integer> result = new ArrayList<>();
         
         Collections.sort(arr);
-        int m, n;
-        for(m=0; m<arr.size(); m+=2){
-                if(arr.size()%2 ==0){
-                   System.out.println(arr.get(m) + "...m");
-                //  result.add((arr.get(m)+arr.get(n))/2);
-                // System.out.println((arr.get(m)+arr.get(n))/2);
+
+        int length = arr.size();
+
+
+        int n,m = length/2;
+        if(length !=0)
+             if(m%2 !=0)
+                for(n=0; n<length-1; n++){
+                    if(length%2 !=0 && length !=0){
+                        result.add((arr.get(n)+arr.get(n+1))/2);
+                    }
+            if(length%2 ==0 )
+                for(n=0; n<=m;n++){
+                    result.add((arr.get(n)+arr.get(n+1))/2);
                 }
-          
+
+    
         }
 
-        System.out.println(result);
-     
+        // System.out.println(result);
+        System.out.println(11/2+1);
     }
 }
