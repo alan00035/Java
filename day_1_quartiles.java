1408130262be13aef1468e65a8c9090b63a98cfc
Tina SONG <alan00035@hotmail.com>
2022-09-02 22:02:06 +1000

    update

--- day_1_quartiles.java
+++ day_1_quartiles.java
@@ -5,32 +5,36 @@ import java.util.List;
 
 public class day_1_quartiles {
     public static void main(String[] args) {
-        Integer[] arr1 ={5,7,1,3};
+        Integer[] arr1 ={5,7,1,3,9};
         
         List<Integer> arr = Arrays.asList(arr1);
         List<Integer> result = new ArrayList<>();
         
         Collections.sort(arr);
 
+        // System.out.println(arr);
         int length = arr.size();
         int n,m = length/2;
-        if(length !=0)
-             if(m%2 !=0)
+
+        // System.out.println(length+" "+m);
+        // if(length !=0)
+             if(length%2 ==0)
                 for(n=0; n<length-1; n++){
                     if(length%2 !=0 && length !=0){
                         result.add((arr.get(n)+arr.get(n+1))/2);
                     }
-            if(length%2 ==0 )
+            if(length%2 ==1 )
                 for(n=0; n<=m;n++){
                     result.add((arr.get(n)+arr.get(n+1))/2);
+                    System.out.println(result);
                 }
-                for(n=m+2; n<length-1;n++){
-                    result.add((arr.get(n)+arr.get(n+1))/2);
+                for(n=length-1; n>m;n--){
+                    result.add((arr.get(n)+arr.get(n-1))/2);
                 }
                 result.add(arr.get(m+1));
         }
 
         // System.out.println(result);
-        System.out.println(11/2+1);
+        // System.out.println(m);
     }
 }
