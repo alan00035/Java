c3795d11149b90c79594ca0322f4b584aaa2bcc9
Tina SONG <alan00035@hotmail.com>
2022-10-19 21:39:15 +1100

    array sorting

--- Insertion_sort_part_1.java
+++ Insertion_sort_part_1.java
@@ -4,20 +4,26 @@ import java.util.List;
 public class Insertion_sort_part_1 {
     public static void main(String[] args) {
         Integer[] arrOrigin ={2,4,6,8,3};
-        int n = arrOrigin.length;
+        int n = arrOrigin.length-2;
         List<Integer> arr = Arrays.asList(arrOrigin);
-
+        
+        int temp = arr.get(n+1);
         int i;
-        for(i=n-1; i>=0; i--){
-            int temp = arr.get(i);
-            // System.out.println(temp);
-            if(temp <arr.get(i)){
-                arr.set(i, arr.get(i));
-                
-            }
+        for(i=n; i>=0; i--){
+            // System.out.println(arr.get(i));
+            if(temp < arr.get(i)){
+                arr.set(i+1, arr.get(i));
+                // arr.forEach(System.out::print);
+                // System.out.println(arr);
 
-           
+            }
+            if(temp >=arr.get(i)){
+                arr.set(i+1, temp);
+                System.out.println(arr);
+                // System.out.println(arr.get(i));
+            }
         }
+        
     }
 }
 
