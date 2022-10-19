8747ed3088b20ab897a4eb1d1c4022b321abbdc8
Tina SONG <alan00035@hotmail.com>
2022-10-20 10:02:40 +1100

    .

--- Insertion_sort_part_1.java
+++ Insertion_sort_part_1.java
@@ -3,28 +3,52 @@ import java.util.List;
 
 public class Insertion_sort_part_1 {
     public static void main(String[] args) {
-        Integer[] arrOrigin ={2,4,6,8,3};
-        int n = arrOrigin.length-2;
+        Integer[] arrOrigin ={2, 3, 4, 5, 6, 7, 8, 9, 10, 1};
+        int count = arrOrigin.length-2;
         List<Integer> arr = Arrays.asList(arrOrigin);
-        
-        int temp = arr.get(n+1);
+
+        int temp = arr.get(count+1);
         int i;
-        for(i=n; i>=0; i--){
-            // System.out.println(arr.get(i));
+        for(i=count; i>=0; i--){
             if(temp < arr.get(i)){
                 arr.set(i+1, arr.get(i));
-                // arr.forEach(System.out::print);
-                // System.out.println(arr);
-
+                for(int c:arr){
+                    System.out.print(c +" ");
+                }
+                // continue;
             }
             if(temp >=arr.get(i)){
                 arr.set(i+1, temp);
-                System.out.println(arr);
-                // System.out.println(arr.get(i));
+            for(int c:arr){
+                    System.out.print(c +" ");
+                }
+                break;
             }
+            System.out.println();
+
+            
         }
-        
     }
+        
 }
 
 // https://www.hackerrank.com/challenges/insertionsort1/problem?isFullScreen=true
+// public static void print(List<Integer> arr){
+//     for(int x: arr){
+//         System.out.print(x+" ");
+//     }
+//     System.out.println();
+// }
+// public static void insertionSort1(int n, List<Integer> arr) {
+//     int key = arr.get(arr.size()-1);
+//     int j = arr.size()-2;
+    
+//     while(j>=0 && arr.get(j)>key){
+//         arr.set(j+1, arr.get(j));
+//         j--;
+//         print(arr);
+//     }
+    
+//     arr.set(j+1, key);
+//     print(arr);
+// }
