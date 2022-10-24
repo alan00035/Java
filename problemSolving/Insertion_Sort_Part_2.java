6620a780d6a3be5d4fb34716b7993991b5eda304
Tina SONG <alan00035@hotmail.com>
2022-10-25 09:18:22 +1100

    .

--- problemSolving/Insertion_Sort_Part_2.java
+++ problemSolving/Insertion_Sort_Part_2.java
@@ -6,7 +6,19 @@ public class Insertion_Sort_Part_2 {
         Integer[] arrOriginal ={3,4,7,5,6,2,1};
         List<Integer> arr = Arrays.asList(arrOriginal);
         int n = arr.size();
-        
-         while()
+        int i =0, temp =0;
+
+         while( i<n ){
+            //  temp = arr.get(i);
+            //  arr.set(i, arr.get(i+1));
+            //  arr.set(i+1, temp);
+            // System.out.println(arr.get(i) +"_" +arr.get(i+1));
+            System.out.println(i);
+             i++;
+            
+
+            //  System.out.print(temp);
+             
+         }
     }
 }
