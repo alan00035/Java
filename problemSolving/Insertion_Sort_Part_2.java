8e267b57de7f7e678afbe324fb396276ccd5316d
Tina SONG <alan00035@hotmail.com>
2022-10-25 19:44:07 +1100

    while loop

--- problemSolving/Insertion_Sort_Part_2.java
+++ problemSolving/Insertion_Sort_Part_2.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Insertion_Sort_Part_2 {
@@ -6,19 +7,23 @@ public class Insertion_Sort_Part_2 {
         Integer[] arrOriginal ={3,4,7,5,6,2,1};
         List<Integer> arr = Arrays.asList(arrOriginal);
         int n = arr.size();
-        int i =0, temp =0;
+        int a;
 
-         while( i<n ){
-            //  temp = arr.get(i);
-            //  arr.set(i, arr.get(i+1));
-            //  arr.set(i+1, temp);
-            // System.out.println(arr.get(i) +"_" +arr.get(i+1));
-            System.out.println(i);
-             i++;
+            for(a =1; a<n; a++){
+                int temp = arr.get(a);
+                int position = a;
             
+                while(temp<arr.get(position-1)){
+                    Collections.swap(arr, position, position-1);
+                    continue;
+                }
+                
+                System.out.println(arr);
+            }
+        
 
-            //  System.out.print(temp);
-             
-         }
-    }
+                // System.out.println(arr);
+            }        
+
+
 }
