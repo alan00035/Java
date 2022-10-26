c0ce76c7e121fecb815cc729ffe1fbd8049711b3
Tina SONG <alan00035@hotmail.com>
2022-10-26 13:12:47 +1100

    .

---
+++ correctness_and_the_loop_invariant.java
@@ -0,0 +1,7 @@
+public class correctness_and_the_loop_invariant {
+    public static void main(String[] args) {
+        
+    }
+}
+
+// https://www.hackerrank.com/challenges/correctness-invariant/problem?isFullScreen=true
