c9b9dc88d17dcc9984c0f9f1d6082fd1317a588c
Tina SONG <alan00035@hotmail.com>
2022-10-28 09:15:19 +1100

    .

--- correctness_and_the_loop_invariant.java
+++ correctness_and_the_loop_invariant.java
@@ -1,6 +1,20 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class correctness_and_the_loop_invariant {
     public static void main(String[] args) {
-        
+        // Scanner scan = new Scanner(System.in);
+        // int a = scan.nextInt();
+        // int[] arr = new int[a-1];
+        // for (int i=0; i<a; i++){
+        //     arr[i] = a;
+        // }
+        int[] arr ={7 ,4 ,3 ,5 ,6, 2} ;
+          Arrays.sort(arr);
+
+          for (int c:arr)
+          System.out.print(c+" ");
     }
 }
 
