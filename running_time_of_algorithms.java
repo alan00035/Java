0f717fb4857f6102956eadd0f1d828597e9dd1aa
Tina SONG <alan00035@hotmail.com>
2022-11-01 13:49:23 +1100

    .

---
+++ running_time_of_algorithms.java
@@ -0,0 +1,7 @@
+public class running_time_of_algorithms {
+    public static void main(String[] args) {
+        
+    }
+}
+
+// hackerrank.com/challenges/runningtime/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
