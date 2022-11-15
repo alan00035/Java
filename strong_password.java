d7aa0f326a1542ad421a512fcf42f84b82d0763d
Tina SONG <alan00035@hotmail.com>
2022-11-15 20:38:39 +1100

    .

---
+++ strong_password.java
@@ -0,0 +1,7 @@
+public class strong_password {
+    public static void main(String[] args) {
+        
+    }
+}
+
+// https://www.hackerrank.com/challenges/strong-password/problem?isFullScreen=true
