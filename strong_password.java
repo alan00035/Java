1be7cbcb6bb2f5c5790d29812b0de00ea3103c89
Tina SONG <alan00035@hotmail.com>
2022-11-16 08:55:55 +1100

    char

--- strong_password.java
+++ strong_password.java
@@ -1,6 +1,21 @@
 public class strong_password {
     public static void main(String[] args) {
+        String password ="ab1";
+        int n = password.length();
+
+        if(!checkLength(n) ){
+            System.out.println(6-n);
+        }
         
+
+    }
+
+    public static boolean checkLength(int n ){
+        if(n<6)
+            return false;
+        else
+            return true;
+
     }
 }
 
