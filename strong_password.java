e56627bca4193d165d3a229a3b17ea07a3c2ef71
Tina SONG <alan00035@hotmail.com>
2022-11-17 09:03:27 +1100

    update '

--- strong_password.java
+++ strong_password.java
@@ -1,3 +1,6 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class strong_password {
     public static void main(String[] args) {
         String password ="ab1";
@@ -9,7 +12,36 @@ public class strong_password {
         
 
     }
+    public static int checkString(String str){
+        int i =0;
+        
 
+        for(char ch : str.toCharArray()){
+            if(!Character.isDigit(ch)){
+                i++;
+            }
+            else if(!Character.isUpperCase(ch)){
+                i++;
+            }
+            else if(!Character.isLowerCase(ch)){
+                i++;
+            }
+            else if(!checkSpecialChar(str)){
+                i++;
+            }
+        }
+
+        return i;
+    }
+
+    public static boolean checkSpecialChar(String s){
+        Pattern p = Pattern.compile("[a-z0-9]", Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(s);
+        if(m.find())
+            return true; //only digital and letters
+        else
+            return false; //contains special char    
+    }
     public static boolean checkLength(int n ){
         if(n<6)
             return false;
