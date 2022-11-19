8343585da5b632d573d28c73f93d39b9033b64fc
alan00035 <alan00035@hotmail.com>
2022-11-19 16:17:59 +1100

    Delete statistics directory

--- statistics/day1_interquartile_range.java
+++
@@ -1,9 +0,0 @@
-package statistics;
-public class day1_interquartile_range {
-    public static void main(String[] args) {
-        
-    }
-}
-
-
-// https://www.hackerrank.com/challenges/s10-interquartile-range/problem?isFullScreen=true
--- statistics/day_1_standard_deviation.java
+++
@@ -1,6 +0,0 @@
-package statistics;
-public class day_1_standard_deviation {
-    
-}
-
-// https://www.hackerrank.com/challenges/s10-standard-deviation/problem?isFullScreen=true
