829e447f97756f1752adde271093223fd86fb053
alan00035 <alan00035@hotmail.com>
2022-11-19 16:16:49 +1100

    Delete running_time_of_algorithms.java

--- running_time_of_algorithms.java
+++
@@ -1,7 +0,0 @@
-public class running_time_of_algorithms {
-    public static void main(String[] args) {
-        
-    }
-}
-
-// hackerrank.com/challenges/runningtime/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
