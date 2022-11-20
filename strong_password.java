cb21d4a2083c54c9b0875839a4e293b2cc83dcd7
Tina SONG <alan00035@hotmail.com>
2022-11-21 08:58:53 +1100

    array

---
+++ strong_password.java
@@ -0,0 +1,8 @@
+public class strong_password {
+    public static void main(String[] args) {
+        
+    }
+}
+
+
+// https://www.hackerrank.com/challenges/strong-password/problem?isFullScreen=true
