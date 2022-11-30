0a551374e0478b485db5ba16d35fc53c6b18a0fc
Tina SONG <alan00035@hotmail.com>
2022-12-01 09:26:26 +1100

    array

--- strong_password.java
+++ strong_password.java
@@ -2,19 +2,40 @@ public class strong_password {
     public static void main(String[] args) {
         String str = "ab1";
         int n = str.length();
+        int gap = 6-n;
+        int missing = checkPassword(str);
         
-        if(n<6){
+        if(gap>missing){
+           System.out.println(gap);
 
         }
         else{
-            
+           System.out.println(missing);
         }
         
     }
     static int checkPassword(String str){
-        
+        int count=0, num=0,u=0,l=0,sp=0;
+        for(char c : str.toCharArray()){
+            if(Character.isDigit(c)){
+                num++;
+            }
+            if(Character.isUpperCase(c)){
+                u++;
+            }
+            if(Character.isLowerCase(c)){
+                l++;
+            }
+            else{
+                sp++;
+            }
+        }
 
-        return 0;
+        if(num==0)count+=1;
+        if(u==0) count+=1;
+        if(l==0) count+=1;
+        if(sp==0) count+=1;
+        return count;
     }
 
     
