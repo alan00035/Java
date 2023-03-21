032c4977dd5fdb7f55956f561e53b5f72acaf0dd
Tina SONG <alan00035@hotmail.com>
2023-03-22 08:48:31 +1100

    data type

--- 30_days_of_code/day1_data_types.go
+++ 30_days_of_code/day1_data_types.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 )
@@ -14,5 +15,20 @@ func datatypes() {
 	var s string = "HackerRank "
 
 	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		scanner.Scan()
+		input := scanner.Text()
+		if len(input) == 0 {
+			break
+		}
+		x, err := strconv.ParseUint(input, 0, 64)
+		y, err := strconv.ParseFloat(input, 64)
+		z := input
 
+		if err != nil {
+			fmt.Println(i + uint64(x))
+			fmt.Println(d + float64(y))
+			fmt.Println(s + " " + z)
+		}
+	}
 }
